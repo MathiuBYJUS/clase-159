5364acdf907e2e19b7304c561e6026427b2ef92f
MathiuBYJUS <95060413+MathiuBYJUS@users.noreply.github.com>
2023-04-03 19:24:46 -0600

    Add files via upload

---
+++ iframe clase 159.py
@@ -0,0 +1,13 @@
+from tkinter import*
+root=Tk()
+frame_1=Frame()
+frame_1.place(relx=0.5 , rely=0.5)
+frame_1.config(bg="blue")
+frame_1.config(width="390" , height="400")
+frame_1.config(bd="30")
+frame_1.config(cursor="")
+
+root.geometry("400x400")
+root.config(background="green")
+
+root.mainloop()
